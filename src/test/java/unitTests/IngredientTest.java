a57b96a23b4f337f0fe9ccc464e8a1af935a0db9
Anastasia Semisorova <semisorova29@mail.ru>
2024-11-16 14:53:04 +0300

    Unit Tests

---
+++ src/test/java/unitTests/IngredientTest.java
@@ -0,0 +1,30 @@
+/*Подключи библиотеки: Jacoco, Mockito, JUnit 4.
+- Покрой тестами класс Ingredient
+- Используй моки, стабы и параметризацию там, где нужно.
+- Процент покрытия должен быть не ниже 70%.
+*/
+package unitTests;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class IngredientTest {
+
+    @Test
+    @Parameters({
+            "SAUCE, ketchup, 10.5",
+            "FILLING, lettuce, 5.0"
+    })
+    public void testIngredientProperties(IngredientType type, String name, float price) {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
+        assertEquals(name, ingredient.getName());
+        assertEquals(price, ingredient.getPrice(), 0.01);
+    }
+}
